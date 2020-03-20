c3d9af7c6fed49ab7ebdff203d3fbd9fc5ff4cfc
amabry-debug <amabry0009@kctcs.edu>
2020-03-20 16:31:37 -0400

    Making a test scene
    this scene has a test scene and test assets for experimenting with the game and code. I also added comments to the lifetime code.

--- Unity File ColdMayhem/Assets/Scripts/Lifetime.cs
+++ Unity File ColdMayhem/Assets/Scripts/Lifetime.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//by Andrew Mabry (Drew)
 
+//this code is used to limit the lifetime of particle affects
 public class Lifetime : MonoBehaviour
 {
+    //declaring variables
     public float lifeTime = 1;
     float timer = 0;
     public ParticleSystem thisParticle;
@@ -11,11 +14,14 @@ public class Lifetime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //counting total time
         timer += Time.deltaTime;
         
+        //if the total time is greater than the lifetime than particles will stop
         if(timer >= lifeTime)
         {
             thisParticle.Stop();
+            //2 seconds after the particles stop the particle object is destroyed
             if(timer >= lifeTime + 2)
                 Destroy(this.gameObject);
         }
