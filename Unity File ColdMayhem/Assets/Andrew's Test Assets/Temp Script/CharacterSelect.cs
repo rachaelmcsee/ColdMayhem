46009bc43386446c546f27f52f669354d95aa148
amabry-debug <amabry0009@kctcs.edu>
2020-04-25 19:26:55 -0400

    Adding character selection
    this adds a character selection menu and a new test player prefab for testing.

---
+++ Unity File ColdMayhem/Assets/Andrew's Test Assets/Temp Script/CharacterSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    //declaring a variable to store the reference to the game info so they can change their character
+    GameInfo info;
+
+    //a variable to store the back button
+    public GameObject back;
+    private void Start()
+    {
+        info = GameObject.FindGameObjectWithTag("Info").GetComponent<GameInfo>();
+    }
+
+    //making a method to change the selected character
+    public void Selecte(int characterInt)
+    {
+        info.playerChoice = characterInt;
+        back.SetActive(true);
+    }
+}
