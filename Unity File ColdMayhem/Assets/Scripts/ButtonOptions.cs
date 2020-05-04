fbf00b10ced07e7a79f8d0f57046898fbaa2beea
Mason Cushing <mac3311mas@hotmail.com>
2020-05-04 18:17:53 -0400

    Addition of Credits menu and clean up of button code as well as screencaps for areneas

--- Unity File ColdMayhem/Assets/Scripts/ButtonOptions.cs
+++ Unity File ColdMayhem/Assets/Scripts/ButtonOptions.cs
@@ -7,7 +7,7 @@ public class ButtonOptions : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(3);
     }
 
     public void ArenaSelect()
@@ -25,19 +25,14 @@ public class ButtonOptions : MonoBehaviour
         SceneManager.LoadScene(2);
     }
 
+    public void Credits()
+    {
+        SceneManager.LoadScene(5);
+    }
+
     //Below here Arena selection buttons
 
-    public void Arena01()
-    {
-        SceneManager.LoadScene(2);
-    }
-
     public void Arena02()
-    {
-        SceneManager.LoadScene(4);
-    }
-
-    public void Arena03()
     {
         SceneManager.LoadScene(4);
     }
@@ -46,9 +41,6 @@ public class ButtonOptions : MonoBehaviour
     {
         SceneManager.LoadScene(3);
     }
-    public void Arena05()
-    {
-        SceneManager.LoadScene(6);
-    }
+
 }
 
