a754d67ff13286101700e08c307af1fa58954d80
amabry-debug <amabry0009@kctcs.edu>
2020-05-05 19:09:25 -0400

    Sound Implementation
    This links sound effects to animations and adds an impact sound for the snowball.

---
+++ Unity File ColdMayhem/Assets/Andrew's Test Assets/Temp Script/animationSounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//by Andrew Mabry
+
+public class animationSounds : MonoBehaviour
+{
+    //declaring public  variables for references
+    public AudioClip[] steps;
+    public AudioClip jump;
+    public AudioClip land;
+    //this is where the sound will play
+    public AudioSource source;
+    //making several methods to be called on animation events
+    public void Step()
+    {
+        source.PlayOneShot(steps[Random.Range(0, steps.Length)]);
+    }
+
+    public void Jump()
+    {
+        source.PlayOneShot(jump);
+    }
+    public void Land()
+    {
+        source.PlayOneShot(land);
+    }
+}
